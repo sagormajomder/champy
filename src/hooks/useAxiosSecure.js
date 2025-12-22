a17070948fb1983c116c55d70341b27e7720ee28
Sagor Majomder <sagormajomder@gmail.com>
2025-12-22 11:43:41 +0600

    Enhance useAxiosSecure hook with request and response interceptors for authorization and error handling

--- src/hooks/useAxiosSecure.js
+++ src/hooks/useAxiosSecure.js
@@ -1,8 +1,49 @@
 import axios from 'axios';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
+import { useAuth } from '../contexts/AuthContext';
 
 const secureInstance = axios.create({
   baseURL: import.meta.env.VITE_SERVER_BASE_LINK,
 });
 export function useAxiosSecure() {
+  const { user, signOutUser } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(
+    function () {
+      // intercept request
+      const reqInterceptor = secureInstance.interceptors.request.use(config => {
+        config.headers.Authorization = `Bearer ${user?.accessToken}`;
+        return config;
+      });
+
+      // interceptor response
+      const resInterceptor = secureInstance.interceptors.response.use(
+        response => {
+          return response;
+        },
+        error => {
+          console.log(error);
+
+          const statusCode = error.status;
+          if (statusCode === 401 || statusCode === 403) {
+            signOutUser().then(() => {
+              navigate('/auth/login');
+            });
+          }
+
+          return Promise.reject(error);
+        }
+      );
+
+      return () => {
+        secureInstance.interceptors.request.eject(reqInterceptor);
+        secureInstance.interceptors.response.eject(resInterceptor);
+      };
+    },
+    [user, signOutUser, navigate]
+  );
+
   return secureInstance;
 }
