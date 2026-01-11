74d7164a59a9ce4ed9c1ebb5dad47cb2a58e0c8f
Sagor Majomder <sagormajomder@gmail.com>
2026-01-11 23:46:51 +0600

    Enhance footer component with contact information and quick links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import { FaTrophy } from 'react-icons/fa';
+import { FiMapPin } from 'react-icons/fi';
+import { TfiEmail } from 'react-icons/tfi';
 import { Link } from 'react-router';
 
 export default function Footer() {
   return (
-    <footer className='footer footer-horizontal footer-center text-white p-10 bg-neutral'>
+    <footer className='footer sm:footer-horizontal footer-vertical text-white p-10 bg-neutral'>
       <aside>
         <p className='font-bold '>
           <span className='text-2xl flex gap-1 items-center'>
@@ -11,13 +13,31 @@ export default function Footer() {
             Champy
           </span>
           <br />
-          {/* Providing reliable tech since 1992 */}
         </p>
+        <div className='flex flex-col gap-2'>
+          <span className='inline-flex items-center gap-1.5'>
+            <FiMapPin className='text-xl' /> Mirpur, Dhaka, Bangladesh
+          </span>
+          <span className='inline-flex items-center gap-1.5'>
+            <TfiEmail className='text-lg' /> sagormajomder@gmail.com
+          </span>
+        </div>
         <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
       </aside>
       <nav>
+        <h6 className='footer-title'>Quick Links</h6>
+        <Link to='/about-us' className='link link-hover'>
+          About Us
+        </Link>
+        <Link to='/contact-us' className='link link-hover'>
+          Contact Us
+        </Link>
+      </nav>
+      {/* Nav */}
+      <nav>
+        <h6 className='footer-title'>Social</h6>
         <div className='grid grid-flow-col gap-4'>
-          <Link>
+          <Link target='_blank' to='https://x.com/sagormajomder'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               width='24'
@@ -27,7 +47,7 @@ export default function Footer() {
               <path d='M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z'></path>
             </svg>
           </Link>
-          <Link>
+          <Link target='_blank' to='https://www.youtube.com/@sagormajomder'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               width='24'
@@ -37,7 +57,7 @@ export default function Footer() {
               <path d='M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z'></path>
             </svg>
           </Link>
-          <Link>
+          <Link target='_blank' to='https://www.facebook.com/sagormajomder/'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               width='24'
